7f7687ca5abaca6737537c004a990c663252a4e1
AhmetAcar <faruk-16@outlook.com>
2021-06-09 16:15:27 +0300

    Added LaunhScreen, Implemented custom font.

---
+++ FlightApp/Pages/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  FlightApp
+//
+//  Created by Ahmet Acar on 9.06.2021.
+//
+
+import UIKit
+
+
+final class LoginViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
